062c58dc4591dd4cb0e1222ca9fd6b514b6e43b4
Nolan Kingdon <nolan.kingdon@gmail.com>
2019-05-14 15:37:27 -0400

    Added fix to stats appearing inversely

--- MMM-DailyPokemon.js
+++ MMM-DailyPokemon.js
@@ -182,7 +182,7 @@ Module.register("MMM-DailyPokemon", {
 			var statTable = document.createElement("table");
 			if(this.config.gbaMode) statTable.style.cssText = "font-family: 'pokegb'";
 			
-			for(let i=0; i<6; i++){
+			for(let i=5; i>=0; i--){//Inverted to list stats in right order
 				let tr = document.createElement("tr");
 				let tdName = document.createElement("td");
 				let tdStat = document.createElement("td");
